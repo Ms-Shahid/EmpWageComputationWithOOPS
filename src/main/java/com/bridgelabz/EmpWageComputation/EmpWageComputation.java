9cf97741178a5e248017dc1d1930290f9afe709c
Ms-Shahid <ms2406shahid@gmail.com>
2021-09-07 23:11:07 +0530

    daily wage for full and part time employee

--- src/main/java/com/bridgelabz/EmpWageComputation/EmpWageComputation.java
+++ src/main/java/com/bridgelabz/EmpWageComputation/EmpWageComputation.java
@@ -1,32 +1,48 @@
 package com.bridgelabz.EmpWageComputation;
 
 public class EmpWageComputation {
-	
-	private int fullTimePresent;
-	private int dailyWage;
+
+	private static final int IS_PRESENT_FULL = 1;
+	private static final int IS_PRESENT_HALF = 2;
+
 	private int fullTimeWork;
+	private int partTimeWork;
+
+	private int dailyWage;
 	static int Wage_Per_Hour = 20;
-	
-	public EmpWageComputation(int fullTimePresent, int fullTimeWork) {
-		this.fullTimePresent = fullTimePresent;
+
+	EmpWageComputation(int fullTimeWork, int partTimeWork) {
+
 		this.fullTimeWork = fullTimeWork;
+		this.partTimeWork = partTimeWork;
 	}
 
-	public void empCheck() {
+	public void empPresence() {
 
-		int empCheck = (int) Math.floor(Math.random() * 10) % 2;
+		int empCheck = (int) Math.floor(Math.random() * 10) % 3;
 
-		if (empCheck == fullTimePresent) {
-			dailyWage = fullTimeWork * Wage_Per_Hour;
-			System.out.println("Employee is present and Daily wage is :" +dailyWage);
-		} else {
-			System.out.println("Employee is absent");
+		switch (empCheck) {
+		case IS_PRESENT_FULL:dailyWage = fullTimeWork * Wage_Per_Hour;
+			System.out.println("Employee is present full time and Daily wage is :" + dailyWage);
+			break;
+			
+		case IS_PRESENT_HALF:dailyWage = partTimeWork * Wage_Per_Hour;
+			System.out.println("Employee is present part time and Daily wage is :" + dailyWage);
+			break;
+			
+		default:
+			System.out.println("Employee is Absent");
+			break;
 		}
 	}
 
 	public static void main(String[] args) {
 
-		EmpWageComputation emp1 = new EmpWageComputation(1,8);
-		emp1.empCheck();
+		EmpWageComputation emp1 = new EmpWageComputation(8, 0);
+		emp1.empPresence();
+
+		EmpWageComputation emp2 = new EmpWageComputation(0, 4);
+		emp2.empPresence();
+
 	}
 }
