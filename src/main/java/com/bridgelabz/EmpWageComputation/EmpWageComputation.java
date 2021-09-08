b18fc133866b3f58021585c1757aba42220f2c3e
Ms-Shahid <ms2406shahid@gmail.com>
2021-09-08 21:43:56 +0530

    wages till a condition is reached

--- src/main/java/com/bridgelabz/EmpWageComputation/EmpWageComputation.java
+++ src/main/java/com/bridgelabz/EmpWageComputation/EmpWageComputation.java
@@ -8,9 +8,9 @@ public class EmpWageComputation {
 	private int fullTimeWork;
 	private int partTimeWork;
 
-	private int dailyWage;
 	static int Wage_Per_Hour = 20;
 	static int max_days =20;
+	static int max_hrs = 100;
 
 	EmpWageComputation(int fullTimeWork, int partTimeWork) {
 
@@ -21,18 +21,31 @@ public class EmpWageComputation {
 	public void empPresence() {
 		
 		int monthlyWage = 0;
-		for(int day=0; day<max_days;day++) {
+		int totalempHrs = 0;
+		int totalFullTimeHrs = 0;
+		int totalPartTimeHrs = 0;
+		int workingDays = 0;
+		
+		while (totalempHrs <= max_hrs && workingDays <= max_days) {
 			
+			workingDays++;
 			int empCheck = (int) Math.floor(Math.random() * 10) % 3; 
 			
 			switch (empCheck) {
-				case IS_PRESENT_FULL:dailyWage = fullTimeWork * Wage_Per_Hour;System.out.println("-------------"+"\n"+"Employee is present full time ");break;
-				case IS_PRESENT_HALF:dailyWage = partTimeWork * Wage_Per_Hour;System.out.println("-------------"+"\n"+"Employee is present part time " );break;
+				case IS_PRESENT_FULL:fullTimeWork+=fullTimeWork;System.out.println("-------------"+"\n"+"Employee is present full time ");break;
+				case IS_PRESENT_HALF:partTimeWork+=partTimeWork;System.out.println("-------------"+"\n"+"Employee is present part time " );break;
 				default:System.out.println("Employee is Absent");break;
 			}
-			monthlyWage += dailyWage;
+			
+			totalFullTimeHrs += fullTimeWork;
+			totalPartTimeHrs += partTimeWork;
+			totalempHrs = totalempHrs + totalFullTimeHrs + totalPartTimeHrs;
+			System.out.println(" Day " + workingDays + ": hours " +totalFullTimeHrs);
+			System.out.println(" Day " + workingDays + ": hours " +totalPartTimeHrs);
 		}
-		System.out.println("Montly wage of Employee is " + monthlyWage);
+		monthlyWage= totalempHrs * Wage_Per_Hour;
+		System.out.println("Monthly Wage of Employee is " + monthlyWage);
+		
 	}
 
 	public static void main(String[] args) {
@@ -42,6 +55,5 @@ public class EmpWageComputation {
 
 		EmpWageComputation emp2 = new EmpWageComputation(0, 4); //partTime
 		emp2.empPresence();
-
 	}
 }
